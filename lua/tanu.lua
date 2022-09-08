5d58e8454df47379ac1f5231924289f91fff8cdb
EpiCanard <epicanard@gmail.com>
2022-09-08 23:48:42 +0200

    feat(deps): add dependency checker with log error

--- lua/tanu.lua
+++ lua/tanu.lua
@@ -1,11 +1,36 @@
 local config = require("tanu.config")
 local commands = require("tanu.commands")
+local log = require("tanu.log")
 
 local M = {}
 
+local function check_dependency(name, url)
+  local res, _ = pcall(require, name)
+  if not res then
+    log.error("Missing dependency '", name, "'. See: ", url)
+  end
+  return res
+end
+
+local function check_dependencies()
+  local deps_loaded = {
+    check_dependency("plenary", "https://github.com/nvim-lua/plenary.nvim")
+  }
+
+  local loaded = true
+  for _, v in ipairs(deps_loaded) do
+    loaded = not v and false or loaded
+  end
+  return loaded
+end
+
+-- Setup tanu plugin
+-- @param user_config Personal config to customize plugin
 function M.setup(user_config)
-  config.setup(user_config)
-  commands.setup()
+  if check_dependencies() then
+    config.setup(user_config)
+    commands.setup()
+  end
 end
 
 return M
---
+++ lua/tanu/log.lua
@@ -0,0 +1,35 @@
+local levels = {
+  error = "ErrorMsg",
+  warn = "WarningMsg",
+  info = "None",
+  debug = "Comment",
+}
+
+local function format_message(...)
+  local msg = ""
+  for _, v in ipairs{...} do
+    if type(v) == "table" then
+        msg = msg .. vim.inspect(v)
+      else
+        msg = msg .. tostring(v)
+      end
+  end
+  return msg
+end
+
+local function log(level)
+  return function(...)
+    local msg = format_message(...)
+    vim.api.nvim_echo({{
+      string.format("[tanu][%s] %s", level:upper(), msg), levels[level]
+    }}, false, {})
+  end
+end
+
+local M = {}
+
+for k, _ in pairs(levels) do
+  M[k] = log(k)
+end
+
+return M
